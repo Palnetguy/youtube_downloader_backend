5f2bc0e66caa89ffb351e755013c492c316a0c67
Martin Rhine <tusingwiremartinrhinetreviz@gmail.com>
2024-05-21 21:20:09 +0300

    added aws elasticbucket

--- youtubedownloader/settings.py
+++ youtubedownloader/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from corsheaders.defaults import default_headers
-import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_0fjmiw_$*_9308xm$4@ym%o!!+nxsco#v5=71jq24g*l94ltn'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
--- youtubedownloader/urls.py
+++ youtubedownloader/urls.py
@@ -19,7 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from backend.views import health_check
+
 urlpatterns = [
+    path('health/', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
